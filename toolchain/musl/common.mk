eab877bcdd39b1870f17c606a7f6dfc5bc207e23
W_Y_CPP <34025388+WYC-2020@users.noreply.github.com>
2022-04-06 11:22:19 +0800

    musl: Disable musl debug

--- toolchain/musl/common.mk
+++ toolchain/musl/common.mk
@@ -37,7 +37,7 @@ MUSL_CONFIGURE:= \
 		--host=$(GNU_HOST_NAME) \
 		--target=$(REAL_GNU_TARGET_NAME) \
 		--disable-gcc-wrapper \
-		--enable-debug \
+		--disable-debug \
 		--enable-optimize
 
 define Host/Configure
